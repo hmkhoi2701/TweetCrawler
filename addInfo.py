0c5c05ef48283eeacf91a55299e07f05729faf35
Ho Minh Khoi <87028107+hmkhoi2701@users.noreply.github.com>
2023-02-02 00:12:57 +0700

    Update addInfo.py

--- addInfo.py
+++ addInfo.py
@@ -1,9 +1,8 @@
 import os
 import pandas as pd
+import sys
 
-with open('/kaggle/working/TweetCrawler/keyword/democrat.txt') as file:
-    keywords = [line.rstrip() for line in file]
-print(keywords)
+keyword = sys.argv[1]
 
 path = "/kaggle/working/TweetCrawler/data"
 # Check whether the path exists or not
@@ -13,34 +12,32 @@ if not os.path.exists(path):
 
 ### Scraping User Profile (Optional) ###
 all_info = []
-for search_words in keywords:
-    filename = search_words.replace(' ', '')
-    info = pd.read_json('/kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['user']]
-    rows = []
-    for idx in range(len(info['user'])):
-        row = info['user'][idx]
-        for key in row.keys():
-            row[key] = [row[key]]
-        rows.append(pd.DataFrame.from_dict(row))
-    all_info.append(pd.concat(rows))
+filename = keyword.replace(' ', '')
+info = pd.read_json('/kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['user']]
+rows = []
+for idx in range(len(info['user'])):
+    row = info['user'][idx]
+    for key in row.keys():
+        row[key] = [row[key]]
+    rows.append(pd.DataFrame.from_dict(row))
+all_info.append(pd.concat(rows))
 info_df = pd.concat(all_info, ignore_index=True)[['id', 'location']]
 
 
 ### Combine all content of tweets and necessary features ###
 frames = []
-for search_word in keywords:
-    filename = search_word.replace(' ', '') 
-    df = pd.read_json('/kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['id', 
-                                                                                            'url', 
-                                                                                            'date', 
-                                                                                            'rawContent',
-                                                                                            'hashtags', 
-                                                                                            'replyCount', 
-                                                                                            'retweetCount', 
-                                                                                            'likeCount']]
-    df['key'] = search_word
-    frames.append(df)
+filename = keyword.replace(' ', '') 
+df = pd.read_json('/kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['id', 
+                                                                                        'url', 
+                                                                                        'date', 
+                                                                                        'rawContent',
+                                                                                        'hashtags', 
+                                                                                        'replyCount', 
+                                                                                        'retweetCount', 
+                                                                                        'likeCount']]
+df['key'] = keyword
+frames.append(df)
 tweets_df = pd.concat(frames, ignore_index=True)
 final = pd.concat([tweets_df, info_df], axis=1)
 final['subject'] = 'Democrat'
-final.to_csv('/kaggle/working/crawl_democrat.csv')
+final.to_csv('/kaggle/working/K_crawl_democrat.csv')
