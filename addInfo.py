87c59a3aac3ecefc099f6576689b447af4f1a6c1
TranafLee <tranthao2492002@gmail.com>
2023-02-01 22:01:55 +0700

    fix 1.x

--- addInfo.py
+++ addInfo.py
@@ -1,11 +1,11 @@
 import os
 import pandas as pd
 
-with open('./keyword/democrat.txt') as file:
+with open('/kaggle/working/TweetCrawler/keyword/democrat.txt') as file:
     keywords = [line.rstrip() for line in file]
 print(keywords)
 
-path = "./data"
+path = "/kaggle/working/TweetCrawler/data"
 # Check whether the path exists or not
 if not os.path.exists(path):
    os.makedirs(path)
@@ -15,7 +15,7 @@ if not os.path.exists(path):
 all_info = []
 for search_words in keywords:
     filename = search_words.replace(' ', '')
-    info = pd.read_json('./raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['user']]
+    info = pd.read_json('/kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['user']]
     rows = []
     for idx in range(len(info['user'])):
         row = info['user'][idx]
@@ -30,7 +30,7 @@ info_df = pd.concat(all_info, ignore_index=True)[['username', 'location']]
 frames = []
 for search_word in keywords:
     filename = search_word.replace(' ', '') 
-    df = pd.read_json('./raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['id', 
+    df = pd.read_json('/kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(filename), lines=True)[['id', 
                                                                                             'url', 
                                                                                             'date', 
                                                                                             'rawContent',
@@ -43,4 +43,4 @@ for search_word in keywords:
 tweets_df = pd.concat(frames, ignore_index=True)
 final = pd.concat([tweets_df, info_df], axis=1)
 final['subject'] = 'Democrat'
-final.to_csv('./data/crawl_democrat.csv')
+final.to_csv('/kaggle/working/crawl_democrat.csv')
--- crawl.py
+++ crawl.py
@@ -3,11 +3,11 @@ import os
 import pandas as pd
 
 
-with open('./keyword/democrat.txt') as file:
+with open('/kaggle/working/TweetCrawler/keyword/democrat.txt') as file:
     keywords = [line.rstrip() for line in file]
 print(keywords)
 
-path = "./raw_data/democrat"
+path = "/kaggle/working/TweetCrawler/raw_data/democrat"
 # Check whether the path exists or not
 if not os.path.exists(path):
    os.makedirs(path)
@@ -21,5 +21,5 @@ until_date = "2022-12-31"
 for search_words in keywords:
     print("Starting process to scrape tweets containing " + search_words + " ...")
     filename = search_words.replace(' ', '')
-    os.system('snscrape --jsonl --max-results {} --since {} twitter-search "{} until:{}"> ./raw_data/democrat/democrat_{}.json'.format(tweet_count, since_date, search_words, until_date, filename))
+    os.system('snscrape --jsonl --max-results {} --since {} twitter-search "{} until:{}"> /kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(tweet_count, since_date, search_words, until_date, filename))
     print("Scraping tweets containing " + search_words + ": DONE")
