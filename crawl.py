1d9f2b46169fd3d5aadea57588582f758eeae3f1
Ho Minh Khoi <87028107+hmkhoi2701@users.noreply.github.com>
2023-02-06 09:00:24 +0700

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -2,18 +2,20 @@ import os
 import pandas as pd
 import sys
 
-keyword = sys.argv[1] + ' ' + sys.argv[2]
+side = sys.argv[1]
 
-path = "/kaggle/working/TweetCrawler/raw_data/democrat"
+keyword = sys.argv[2] + ' ' + sys.argv[3]
+
+path = f"/kaggle/working/TweetCrawler/raw_data/{side}"
 # Check whether the path exists or not
 if not os.path.exists(path):
    os.makedirs(path)
 
 tweet_count = int(1e7)
-since_date = "2022-10-01"   
-until_date = "2022-11-09"
+since_date = sys.argv[4]   
+until_date = sys.argv[5]
 
 print("Starting process to scrape tweets containing " + keyword + " ...")
 filename = keyword.replace(' ', '')
-os.system('snscrape --jsonl --max-results {} --since {} twitter-search "{} until:{}"> /kaggle/working/TweetCrawler/raw_data/democrat/democrat_{}.json'.format(tweet_count, since_date, keyword, until_date, filename))
+os.system('snscrape --jsonl --max-results {} --since {} twitter-search "{} until:{}"> /kaggle/working/TweetCrawler/raw_data/{}/{}.json'.format(tweet_count, since_date, keyword, until_date, side, filename))
 print("Scraping tweets containing " + keyword + ": DONE")
